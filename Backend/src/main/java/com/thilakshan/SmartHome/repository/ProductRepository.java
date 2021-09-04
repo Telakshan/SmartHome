d99e44fb93e15f678eac0ab9d077468346fdab74
Thilakshan Baskaran <telakshanb@gmail.com>
2021-09-04 18:26:47 -0400

    added mysql database support

--- Backend/src/main/java/com/thilakshan/SmartHome/repository/ProductRepository.java
+++ Backend/src/main/java/com/thilakshan/SmartHome/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import com.thilakshan.SmartHome.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,4 +17,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
     Page<Product> findByNameContainingIgnoreCase(@RequestParam("name") String name, Pageable pageable);
+
 }
